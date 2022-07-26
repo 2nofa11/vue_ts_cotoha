0d36b16b65aa84c9772a88a4aeb7c0cdcd763779
shava2Curry <47783146+shava2C@users.noreply.github.com>
2022-07-26 09:19:48 +0900

    GitHub Pages用の設定情報の追加

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: '/vue_ts_cotoha/',
+  outputDir: 'docs',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
