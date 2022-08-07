84bd22d9392bea769bd9820d9186e366c17b8d0b
2nofa11 <aboubthethirdkimi@icloud.com>
2022-08-07 14:25:02 +0900

    #9 対応　defineComponent

--- src/components/molecules/SubmitForm.vue
+++ src/components/molecules/SubmitForm.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script lang="ts">
-  import {defineComponent} from "vue"
+  import { defineComponent } from "vue"
   import DoubleIconButton from "../atoms/DoubleIconButton.vue"
   import axios from "axios"
 
