860cbdbf1e5954a98f5522d92a7f3fbb89503a98
2nofa11 <aboubthethirdkimi@icloud.com>
2022-08-12 17:21:44 +0900

    #11 条件式の並び順変更

--- src/components/molecules/SubmitForm.module.ts
+++ src/components/molecules/SubmitForm.module.ts
@@ -1,4 +1,4 @@
 // テキストが空文字または、想定された禁止文字列だった場合はTRUEを返却
 export const is_correctTextInfo = (inputText: string, defaultText: string) => {
-  return !inputText || inputText == defaultText ? true : false
+  return inputText == "" || inputText == defaultText ? true : false
 }
