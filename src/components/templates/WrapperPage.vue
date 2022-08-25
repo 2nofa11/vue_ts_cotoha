153efa144f8e9a5c77e57588d9a701a68bf16695
2nofa11 <aboubthethirdkimi@icloud.com>
2022-08-25 18:56:14 +0900

    ヘッダー部分にrouter-linkを移動

--- src/components/templates/WrapperPage.vue
+++ src/components/templates/WrapperPage.vue
@@ -1,10 +1,13 @@
 <template>
-  <Header></Header>
+  <Header>
+    <v-slot>
+      <router-link to="/about">About</router-link>
+      <router-link to="/vue_ts_cotoha">Use</router-link>
+    </v-slot>
+  </Header>
   <v-main class="grey lighten-3">
     <v-container>
       // TODO画面遷移したらコンポーネントが消えるのは修正Vuex使う？
-      <router-link to="/about">About</router-link>
-      <router-link to="/vue_ts_cotoha">Use</router-link>
       <router-view></router-view>
     </v-container>
   </v-main>
